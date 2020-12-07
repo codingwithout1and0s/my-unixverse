7a829b17615b8ea750ab230de9b33d0ba50a2708
Tang Newyen <codingwithout1and0s@gmail.com>
2020-12-07 16:55:17 -0600

    fixed Netlify deployment error

--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -11,15 +11,27 @@ const About = () => {
       <SEO title='About Me' />
       <Container>
         <h1 className={responsiveTitle1}>About Me</h1>
-        <p>Culpa aliqua do veniam proident et sunt veniam eu amet do. Exercitation cillum irure elit anim exercitation occaecat commodo labore duis. Non voluptate mollit velit proident ea ad amet laborum dolor deserunt consectetur aliquip occaecat. Ea irure culpa sit laborum eiusmod.
+        <p>同色顧，目社們全，心領情國東傳產制不像話洋、在機事：境道不男子子王種，她許系路國樂型一個民影孩健己為來只市的千人狀希然要就好算有可為？大機政不稱創法務多生英河驚在案身樂家和體！問假色廣觀顯女。
 
-          Excepteur sit incididunt ex do culpa officia. Minim fugiat officia exercitation incididunt eiusmod id adipisicing veniam. Occaecat nostrud velit deserunt ut ipsum laborum exercitation enim sit aute.
+的安一標同我樂來花大回能在於口：列在集，義模的告大與總會見放白達環母，行另影賽，她不要的音為定社團二人法應提心密世社，德位力運價可辦論注滿決上美紀異是具如最內生回下聲亞給時月文得位益拿男可。玩學市覺母，他阿先最力？有算建土能道親表，聽興業火任。
 
-          Pariatur exercitation nisi excepteur proident velit ut sunt magna laborum ut velit ad. Fugiat velit ullamco anim minim duis occaecat labore non amet. Velit ad aute eiusmod cillum sunt nulla minim non dolor adipisicing nostrud ullamco non fugiat. Non non fugiat dolor aliqua excepteur exercitation aliqua ea in consequat id. Consequat veniam exercitation cupidatat voluptate magna culpa voluptate dolore incididunt nulla laboris adipisicing consectetur.
+立結師史火不卻起行供象育這立政布族過嚴你無清中得人聲單，管戲再後不、國建想子力回。像引明由了……的東的定少；價不論我臺他不上？
 
-          Sit cillum irure elit occaecat esse elit magna consequat amet dolore esse quis minim. Lorem in nulla proident eu ea sunt adipisicing laboris pariatur ullamco nulla. Velit quis officia quis aute aliquip commodo eiusmod in laborum. Aliquip ad in elit cillum quis. Consequat excepteur dolore Lorem mollit consectetur esse magna do aliqua non sint non voluptate. Enim pariatur in ipsum laboris nulla. Elit non occaecat anim sit ullamco non duis.
+結們們是權，得的消大重許樂不都富，種主密於們汽金卻功之感？推卻業女食我特間過上大，能依市，原外出展那裡景是音產個之間發不兒自和、何角里……開的分縣智有得個大無學像否不建公園夫吃電邊……人王己洋本打興怎影天開跟來感臉水父？土質找眼功縣聯、益供形道老有我以腦力立高留型此小麼的實食又步地如親布：吃生達期要進器也的定了功整事出不學工中容理。白成會以會隨處管，他來土其健下成背無，傳紀汽地：說把登愛為的童做足其有太自電入實但旅心……世次存大子生人大者排，者出臺有家能流不人人年生重。
 
-          Adipisicing tempor eu qui proident exercitation. Duis laborum sit eu magna ut Lorem consectetur ut consequat aute id pariatur cillum ex. Minim minim duis ad duis ad incididunt est exercitation ipsum nostrud. Excepteur exercitation veniam sunt ad nisi labore ipsum nisi sunt sit veniam irure. Amet voluptate ad duis qui elit Lorem aliqua commodo dolor ad.</p>
+注還西表人生。
+
+而存命向許下是別政程隨子感在時濟。家來他學輪是花住龍一法的黃無的雄以產可管們難展；中人結留活些門來市還打定由。立士景！為發英河他對們市假說業士友。找愛華太所，件兒我不國是藥間人，媽是持他，上人小進提由，後而是白只前片上維看一決覺母唱流防處青漸單？世坡設一科古定科上臺字室對我由分亮。備作。
+
+上體大斯國充地；要來斷小定問。
+
+和香體上天，說故類總。成直驚書味獲？
+
+一史合不進綠著德黃化我，年人都內一式好都府益一多良再頭身；準地活看活去房朋送。說兩開，至色生文生然廣故不前品外驗不防落證臺打變是。
+
+策洲醫委建國農花就久經器？坡會風達臺是路像，士人子技我為。為來會再會深務展地歷兒就影信時面人海亮！士了通有布火出水，子上成者國民除是自的天北不圖司勢的人來老化真現起用縣黨復親竟類有消縣發輪賽團的了連目。多電人地，影設小影使：一論院現兒中來當調樹適大決，工居然，眼定只確興的公很放師，人的影們事題有她主親亮位一這會味父之好易……說供向女還信，可師個馬會正，政己感夠是戰工此然個者岸認望被小正的。兒完一之在裡福無球望案你造時信持策再生出一起無二一，長基道增相運國地書麼久故法再就才火式目光維支裡方小改們不……創在明久子來負媽一取！
+
+廣不本得，商家日：請爭獲戲市便健主有生：形它特？和他些國英臺府立題作亞？站高原畫話藥是看綠學化性。在看辦然這終支？了文親人共受用子吃在時成下建是病歷現美媽媽除反過分語人在至別；參回國男產多我行統然，時期主，教生得，資印和以科一官臺市那動？</p>
       </Container>
     </Layout>
   )
