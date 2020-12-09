1719598f48770b351e8f2b3854f79389339038dc
Tang Newyen <codingwithout1and0s@gmail.com>
2020-12-09 13:32:31 -0600

    Updated Aside

--- web/src/components/aside.js
+++ web/src/components/aside.js
@@ -2,25 +2,28 @@ import React from 'react'
 import styled from 'styled-components'
 import ProfilePicture from '../assets/sung-wang.jpg'
 
+import './aside.css'
+
 const AsidePanel = styled.div`
+display: flex
+content-self: center
+content-items: center
+text-align: center
 width: 100px
 height: 320px
-outline: 1px solid green
-`
-
-const ProfileIMG = styled.img`
-max-width: 100%
-height: 200px
-border: 1px solid #ddd
-border-radius: 4px
-padding: 5px
+font-size: 0.8em
+border: 1px solid red
 `
 
 function Aside () {
   return (
     <AsidePanel>
       <p>Hi. I'm Tang</p>
-      <ProfileIMG src={ProfilePicture} alt='Sung Wang' />
+      <img src={ProfilePicture} alt='Sung Wang' />
+      <p>Aspiring Dev | Javascript Monkey</p>
+      <p>It's never too late to turn a hobby into a career</p>
+      <p>
+        My-Unixverse is not a brand. There's no sales funnel. It's Just a portal of raw ideas and experiences shared with you through the interwebs.  I am simply human, full of ideas and faults. Most of the time you'll see my developer journey. Then there's those moments where I might share my dirty laundry.</p>
     </AsidePanel>
   )
 }
