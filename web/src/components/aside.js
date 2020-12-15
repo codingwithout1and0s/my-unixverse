494767ce2265fa891d6005164f6ea9cf5cbf9d62
Tang Newyen <codingwithout1and0s@gmail.com>
2020-12-15 14:33:52 -0600

    removed styled components

--- web/src/components/aside.js
+++ web/src/components/aside.js
@@ -1,34 +1,23 @@
 import React from 'react'
-import styled from 'styled-components'
+
 import ProfilePicture from '../assets/sung-wang.jpg'
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faGithub} from '@fortawesome/fontawesome-free'
 
-import './aside.css'
-
-const AsidePanel = styled.div`
-display: flex
-content-self: center
-content-items: center
-text-align: center
-width: 100px
-height: 320px
-font-size: 0.8em
-border: 1px solid red
-`
+import styles from './aside.module.css'
 
 function Aside () {
   return (
-    <AsidePanel>
+    <div className={styles.root}>
       <p>Hi. I'm Tang</p>
-      <img id="ProfilePicture" src={ProfilePicture} alt='Sung Wang' />
+      <img id={styles.ProfilePicture} src={ProfilePicture} alt='Sung Wang' />
       <FontAwesomeIcon icon={faGithub} size='1x' />
       <p>Aspiring Dev | Javascript Monkey</p>
       <p>It's never too late to turn a hobby into a career</p>
       <p>
         My-Unixverse is not a brand. There's no sales funnel. It's Just a portal of raw ideas and experiences shared with you through the interwebs.  I am simply human, full of ideas and faults. Most of the time you'll see my developer journey. Then there's those moments where I might share my dirty laundry.</p>
-    </AsidePanel>
+    </div>
   )
 }
 
--- web/src/components/footer.js
+++ web/src/components/footer.js
@@ -1,20 +1,15 @@
 import React from 'react'
-import styled from 'styled-components'
 
-const FooterBar = styled.div`
-text-align: center;
-background-color: rgba(20, 20, 20, 0.1);
-color: #000;
-`
+import styles from './footer.module.css'
 
 const FooterComp = () => {
   return (
-    <FooterBar>
+    <div className={styles.root}>
       &copy; {new Date().getFullYear()}, Built with <a href='https://www.sanity.io'>Sanity</a>{' '}
       &amp;
       {` `}
       <a href='https://www.gatsbyjs.org'>Gatsby</a>
-    </FooterBar>
+    </div>
   )
 }
 
